3c453828392857fcbe8c478eb6d7d384fe208d70
Athanasios Anastasiou <athanastasiou@gmail.com>
2023-04-12 16:06:12 +0000

    Corrected tests to not abort when (optional) pandas is not installed

--- test/test_CompositeArrayObject.py
+++ test/test_CompositeArrayObject.py
@@ -5,12 +5,11 @@ Tests functionality that is specific to ``CompositeArrayObject`` objects.
 :date: March 2019
 """
 
-from neoads import CompositeArrayObjectDict, CompositeArrayObjectList, CompositeArrayObjectDataFrame
+from neoads import CompositeArrayObjectDict, CompositeArrayObjectList
 from neoads import ElementDomain
 import neomodel
 import random
-import pandas
-
+import warnings
 
 class DBItem(ElementDomain):
     simple_id = neomodel.UniqueIdProperty()
@@ -19,15 +18,22 @@ class DBItem(ElementDomain):
     pos_y = neomodel.IntegerProperty()
 
 
-def test_CompositeArrayObjectDataFrame():
-    items = [DBItem(ob_tag=f"DBItem_{k}", pos_x=random.randint(0,100), pos_y = random.randint(0,100)).save() for k in range(0,10)]
-    query = CompositeArrayObjectDataFrame("MATCH (a:DBItem) WHERE a.ob_tag=~'DBItem_[0-9]+' RETURN a.pos_x as pos_x, a.pos_y as pos_y").save()
+try:
+    import pandas
+    from neoads import CompositeArrayObjectDataFrame
 
-    ret_data = query.execute()
+    def test_CompositeArrayObjectDataFrame():
+        items = [DBItem(ob_tag=f"DBItem_{k}", pos_x=random.randint(0,100), pos_y = random.randint(0,100)).save() for k in range(0,10)]
+        query = CompositeArrayObjectDataFrame("MATCH (a:DBItem) WHERE a.ob_tag=~'DBItem_[0-9]+' RETURN a.pos_x as pos_x, a.pos_y as pos_y").save()
+    
+        ret_data = query.execute()
+    
+        assert type(ret_data) is pandas.DataFrame
+        assert len(ret_data) == len(items)
+    
+        query.delete()
+        for item in items:
+            item.delete()
+except ImportError as e:
+    warnings.warn("WARNING!!! pandas is not installed, CompositeArrayObjectDataFrame not tested")
 
-    assert type(ret_data) is pandas.DataFrame
-    assert len(ret_data) == len(items)
-
-    query.delete()
-    for item in items:
-        item.delete()

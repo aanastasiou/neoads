dc23e022f99c068d844b3e23319967e6b5ac1ebd
Athanasios Anastasiou <athanastasiou@gmail.com>
2023-06-13 15:34:10 +0000

    Minor correction to the abstractset docs

--- neoads/ads_abstractset.py
+++ neoads/ads_abstractset.py
@@ -30,6 +30,7 @@ An ``AbstractSet`` is represented in Neo4j as follows:
 
     }
 
+
 * Where ``PersistentElement`` can be **ANY** entity in the data model deriving from ``PersistentElement``.
   For more details please see :ref:`datamodeling` 
 
